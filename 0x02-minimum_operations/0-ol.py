65accdfb2982f33bf2e69e7ff3ad4ced56eb6039
sofoniasgd <zephaniahsofa@gmail.com>
2024-08-23 04:20:48 +0300

    updated task 0

--- 0x02-minimum_operations/0-ol.py
+++ 0x02-minimum_operations/0-ol.py
@@ -1,15 +1,43 @@
 #!/usr/bin/python3
-'''Minimum Operations python3 challenge'''
+"""0. Minimum Operations"""
 
+import math
+import primefac
 
 def minOperations(n):
-    '''calculates the fewest number of
-    operations needed to result in exactly n H
-    characters in this file.
-    Returns:
-        Integer : if n is impossible to achieve, return 0
-    '''
-    pasted_chars = 1  # how many chars in the file
+    """In a text file, there is a single character H.
+        Your text editor can execute only two operations in this file:
+            Copy All and Paste. 
+        Given a number n, this method calculates the fewest number of 
+        operations needed to result in exactly n H characters in the file.
+        Args:
+            n: int
+        Returns: int
+        Example:
+            n = 9
+            H => Copy All => Paste => HH => Paste =>HHH => Copy All =>
+            Paste => HHHHHH => Paste => HHHHHHHHH
+            Number of operations=6
+    """
+    # solution #1 using prime facorization
+    # the summ of all the prime factors is the same as the minimum number
+    # of operations to get 'n' number of H characters
+    sum_of_pime_factors = []
+    factors = [2]
+    # print("factors", factors, type(factors))
+    """
+    if n <= 3:
+        return
+    for pf in range(3, math.floor(n / 2), 2):
+        for i in range(2, pf):
+            if pf % i == 0:
+                break
+            else:
+                if pf not in factors:
+                    factors.append(pf)
+    """
+    # print("factors list", primefac(n))
+     pasted_chars = 1  # how many chars in the file
     clipboard = 0  # how many H's copied
     counter = 0  # operations counter
 
