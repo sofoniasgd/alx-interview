982c4d12e287a17862bf9474dd4ca8b5733106f4
sofoniasgd <zephaniahsofa@gmail.com>
2024-08-30 10:46:26 +0300

    updated task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,64 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+""" Log parsing
+    A script that reads stdin line by line and computes metrics:
 
+    Input format:
+    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code>
+    <file size>
+    (if the format is not this one, the line must be skipped)
+    After every 10 lines and/or a keyboard interruption (CTRL + C),
+    print these statistics from the beginning:
+        Total file size: File size: <total size>
+        where <total size> is the sum of all previous <file size>
+            (see input format above)
+        Number of lines by status code:
+        possible status code:
+            200, 301, 400, 401, 403, 404, 405 and 500
+        if a status code doesn’t appear or is not an integer,
+        don’t print anything for this status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
+"""
 
+import signal
 import sys
+from time import sleep
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
 try:
+    # define status codes with counters and a line counter
+    codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+             "405": 0, "500": 0}
+    file_size = 0
+    line_counter = 0
+
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
-
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+        # for every line either extract data or print if 10th line is reached
+        if line_counter == 10:
+            # print_report
+            sys.stdout.write("File size: {}\n".format(file_size))
+            sys.stdout.flush()
+            for key, value in codes.items():
+                if value > 0:
+                    sys.stdout.write("{}: {}\n".format(key, value))
+                    sys.stdout.flush()
+            line_counter = 0
+        else:
+            line_counter += 1
+            # update_report
+            word_list = line.split(" ")
+            if len(word_list) == 9:
+                # update status data
+                file_size += int(word_list[-1])
+                if word_list[-2] in codes.keys():
+                    codes[word_list[-2]] += 1
+    sleep(1)
+except KeyboardInterrupt:
+    # crrl-c pressed, print report and exit
+    sys.stdout.write("File size: {}\n".format(file_size))
+    sys.stdout.flush()
+    for key, value in codes.items():
+        if value > 0:
+            sys.stdout.write("{}: {}\n".format(key, value))
+            sys.stdout.flush()
+    sys.stdout.flush()
