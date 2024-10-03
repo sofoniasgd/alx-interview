cf3c8a97acababc9707feab559b2e97a3ffd974f
sofoniasgd <zephaniahsofa@gmail.com>
2024-10-03 15:53:25 +0300

    added task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""0. Change comes from within"""
+
+
+def makeChange(coins, total):
+    """determines the fewest number of coins needed to meet a given amount
+        Args:
+            coints (list) list of different coin values
+            total (int) the total amount
+        Returns:
+            fewest number of coins needed to meet total
+    """
+    # sort the coin value list Descending
+    coins.sort(reverse=True)
+    total = total
+    # Algorithm
+    # number of coins is found by starting division by the
+    # largest value, storing quotient(number of coins of the current value)
+    # and then dividing the remainder by the next smaller value
+    # if there exists a remainder after the last diviion(smallest coin value)
+    # then theres no solution
+    amount = 0
+    if coins is None or total < coins[-1]:
+        return -1
+    for value in coins:
+        remainder = total % value
+        if coins[-1] == value and remainder != 0:
+            return -1
+        amount += total // value
+        total = remainder
+    return amount
